ddded8dbee9acda58e519c8fbb35ed8056cbba68
raeesiqbal <rayiszafar@gmail.com>
2024-06-27 09:28:22 +0500

    apis are added

--- crypto/urls.py
+++ crypto/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import (
-    HomeView,
-)
+from . import views
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
+    path('', views.HomeView.as_view(), name='home'),
+    path('api/block_account/', views.get_block_account, name='block_account'),
+    path('api/latest_hash/', views.get_latest_hash, name='latest_hash'),
+    path('api/bc_per_block/', views.get_bc_per_block, name='bc_per_block'),
+    path('api/difficulty/', views.get_difficulty, name='get_difficulty'),
+    path('api/total_bc/', views.get_total_bc, name='get_total_bc'),
+    path('api/probablity/', views.get_probablity, name='get_probablity'),
+    path('api/hash_to_win/', views.get_hash_to_win, name='get_hash_to_win'),
+    path('api/next_re_target/', views.get_next_re_target, name='get_next_re_target'),
+    path('api/unconfirmed_count/', views.get_unconfirmed_count, name='get_unconfirmed_count'),
+    path('api/market_cap/', views.get_market_cap, name='get_market_cap'),
+    path('api/stats/', views.get_stats, name='get_stats'),
+    path('api/24_hour_btcsent/', views.get_24_hour_btcsent, name='get_24_hour_btcsent'),
 ]
--- crypto/views.py
+++ crypto/views.py
@@ -1,6 +1,8 @@
 # Imports
 from django.views.generic import View
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import requests
 
 
 # Views
@@ -8,3 +10,111 @@ class HomeView(View):
     def get(self, request, *args, **kwargs):
         context = {}
         return render(request, "crypto/home.html", context=context)
+
+
+def get_block_account(request):
+    response = requests.get("https://blockchain.info/q/getblockcount")
+    data = response.text
+    data = {
+        'content': data
+    }
+    return JsonResponse(data)
+
+
+def get_latest_hash(request):
+    response = requests.get("https://blockchain.info/q/latesthash")
+    data = response.text
+    data = {
+        'content': data
+    }
+    return JsonResponse(data)
+
+
+def get_bc_per_block(request):
+    response = requests.get("https://blockchain.info/q/bcperblock")
+    data = response.text
+    data = {
+        'content': data
+    }
+    return JsonResponse(data)
+
+
+def get_difficulty(request):
+    response = requests.get("https://blockchain.info/q/getdifficulty")
+    data = response.text
+    data = {
+        'content': data
+    }
+    return JsonResponse(data)
+
+
+def get_total_bc(request):
+    response = requests.get("https://blockchain.info/q/totalbc")
+    data = response.text
+    data = {
+        'content': data
+    }
+    return JsonResponse(data)
+
+
+def get_probablity(request):
+    response = requests.get("https://blockchain.info/q/probability")
+    data = response.text
+    data = {
+        'content': data
+    }
+    return JsonResponse(data)
+
+
+def get_hash_to_win(request):
+    response = requests.get("https://blockchain.info/q/hashestowin")
+    data = response.text
+    data = {
+        'content': data
+    }
+    return JsonResponse(data)
+
+
+def get_next_re_target(request):
+    response = requests.get("https://blockchain.info/q/nextretarget")
+    data = response.text
+    data = {
+        'content': data
+    }
+    return JsonResponse(data)
+
+
+def get_unconfirmed_count(request):
+    response = requests.get("https://blockchain.info/q/unconfirmedcount")
+    data = response.text
+    data = {
+        'content': data
+    }
+    return JsonResponse(data)
+
+
+def get_market_cap(request):
+    response = requests.get("https://blockchain.info/q/marketcap")
+    data = response.text
+    data = {
+        'content': data
+    }
+    return JsonResponse(data)
+
+
+def get_stats(request):
+    response = requests.get("https://api.blockchain.info/stats")
+    data = response.text
+    data = {
+        'content': data
+    }
+    return JsonResponse(data)
+
+
+def get_24_hour_btcsent(request):
+    response = requests.get("https://blockchain.info/q/24hrbtcsent")
+    data = response.text
+    data = {
+        'content': data
+    }
+    return JsonResponse(data)
